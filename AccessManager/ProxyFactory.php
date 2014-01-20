66241ba849c59a04b6bda1e61ca2d71b9a636184
Guillaume Royer <guilro@redado.org>
2014-01-20 23:42:53 +0100

    add licence

--- AccessManager/ProxyFactory.php
+++ AccessManager/ProxyFactory.php
@@ -1,21 +1,21 @@
 <?php
 /*
- * This file is part of Redado.
+ * This file is part of GuilroProtectionProxyBundle.
  *
  * Copyright (C) 2013 Guillaume Royer
  *
- * Redado is free software: you can redistribute it and/or modify
+ * GuilroProtectionProxyBundle is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Redado is distributed in the hope that it will be useful,
+ * GuilroProtectionProxyBundle is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Redado.  If not, see <http://www.gnu.org/licenses/>.
+ * along with GuilroProtectionProxyBundle.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Guilro\ProtectionProxyBundle\AccessManager;
